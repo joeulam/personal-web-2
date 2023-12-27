22be47ddbd6eced5938cbdd6e77221ce02151545
Joey Lam <joeulam345@gmail.com>
2023-12-26 20:29:00 -0500

    Background update

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,73 @@
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
+      <Image
+        className='Bg'
+          src="/IMG_1004.img"
+          width={1728}
+          height={1117}
+          alt='/image/IMG_1004.jpg'
+        
+        
+        />
+        <div className="sticky">
+          <div className="header grid gap-4 content-center">
+              <ul>
+                <li><a href="#Mids">Home</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Resume">Resume</a></li>
+                <li><a href="#Projects">Projects</a></li>
+                <li><a href="#Contact">Contact</a></li>
+              </ul>
+            </div>
+          <hr/>
+        </div>
+        
+        
+
+          
+
+          <div id="Mids">
+            <h1 className="Name">Joey Lam</h1>
+          </div>
+
+          <div className="Descdes">
+            <div className="Desc">
+              Student
+            </div>
+            <div className="Desc">
+              Software Engineer
+            </div>
+            <div className="Desc">
+              Inventor
+            </div>
+          </div>
+          
+          <div className='Socials'>
+
+          </div>
+
+
+
+
+        <div id="About" className="block">
+          <h1>About me</h1>
+        </div>
+
+        <div id="Resume" className="block">
+          <h1 >Resume</h1>
+        </div>
+
+        <div id="Projects" className="block">
+          <h1>Projects</h1>
+        </div>
+
+        <div id="Contact" className="block">
+          <h1>Contact</h1>
+        </div>
       
-      <div className="sticky">
-        <div className="header grid gap-4 content-center">
-            <ul>
-              <li><a href="#Mids">Home</a></li>
-              <li><a href="#About">About</a></li>
-              <li><a href="#Resume">Resume</a></li>
-              <li><a href="#Projects">Projects</a></li>
-              <li><a href="#Contact">Contact</a></li>
-            </ul>
-          </div>
-        
-        <hr/>
-      </div>
-      <div id="Mids">
-
-      </div>
-
-
-
-      <div  className="Mid">
-
-        <div >
-          <h1 className="Name">Joey Lam</h1>
-        </div>
-
-        <div className="Descdes">
-          <div className="Desc">
-            Student
-          </div>
-          <div className="Desc">
-            Software Engineer
-          </div>
-          <div className="Desc">
-            Inventor
-          </div>
-        </div>
-        
-        <div className='Socials'>
-        </div>
-
-      </div>
-
-      <div className="block">
-        <h1 id="Resume">Resume</h1>
-      </div>
-      <div id="Projects" className="block">
-        Projects
-      </div>
-      <div id="Contact" className="block">
-        Contact
-      </div>
       <div>
         <footer>
 
