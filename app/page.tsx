48c3f99bb7f4b307f1602e95daa0a300cace08d2
Joey Lam <joeulam345@gmail.com>
2023-12-29 17:56:21 -0500

    Completed about me block

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,9 @@ export default function Home() {
         
         
         />
-        <div className="sticky">
+
+
+        <div className="stickys">
           <div className="header grid gap-4 content-center">
               <ul>
                 <li><a href="#Bgs">Home</a></li>
@@ -45,6 +47,9 @@ export default function Home() {
             </div>
           <hr/>
         </div>
+
+
+
 
         <div className=''>
           <div id="Mids">
@@ -127,14 +132,16 @@ export default function Home() {
           </Box>
         </div>
 
-        <div id="About" className="block">
+        <div id="About" className="h-[140vh] bg-[#03071e]">
           <h1 className='AboutTitle'>About me</h1>
           <div className='mt-20 photoBlock'>
             <div className='avatar mb-10'>
-              <Avatar alt="Remy Sharp" src="/IMG_2811.jpg" sx={{ zIndex:'5', width: '20%', height: '20%' }}/>
+              <Avatar alt="Remy Sharp" src="/IMG_2811.jpg" sx={{zIndex: '7', width: '20%', height: '20%' }}/>
             </div>
+
+
             <div>
-              <p className='AboutDesp'>
+              <p className='AboutDesp ml-10'>
                 I'm a student that is heavily intrested in the CS/Finanical
                 sector. I'm also a current Computer science and Economics major
                 at Boston University, class of 2027.
@@ -143,71 +150,105 @@ export default function Home() {
           </div>
 
 
-          <div>
-            <div className='SkillsBlock'>
-              <h2>Skills</h2>
-              <p>I'm a highly intergratable teamworker that is strong in research and coding. My areas of expertise are computer science and economics.</p>
-              
-              
-              <div className='ml-20 '>
-                <div>
-                  MS Suite
-                  <Box sx={{ zIndex:'5', color:"black", width: 500 , height: 10 }}>
-                    <LinearProgress value={90} variant="determinate" />
-                  </Box>
-                </div>
+          <div className='z-[1] SkillsBlock relative ml-40 mt-20'>
+            <div className='SkillsBlockL absolute left-40 top-0'>
+                <h2>Profile</h2>
+                <hr className='w-[20vw]'/>
+                <p>Currently looking for a internship. If you think I'm a good fit, please contant me via joey.lamm@outlook.com.</p>
+              </div>
+
+              <div className='SkillsBlockR absolute mr-20 right-60'>
+                <h2>Skills</h2>
+                <hr className='w-[30vw]'/>
+                <p>I'm a highly intergratable teamworker that is strong in research and coding. My areas of expertise are computer science and economics.</p>
                 
+                
+                <div className=''>
+                  <div>
+                    <div>
+                      MS Suite
+                    </div>
+                    <div className='inline-block'>
+                      <Box sx={{ zIndex:'5', color:"black", width: 400 , height: 10 }}>
+                        <LinearProgress value={90} variant="determinate" />
+                      </Box>
+                    </div>
+                    <p className='inline-block ml-5'>90%</p>
 
-                <div>
-                  Python
-                  <Box sx={{ zIndex:'5', width: 500 , height: 10 }}>
-                    <LinearProgress value={70} variant="determinate" />
-                  </Box>
-                </div>
+                  </div>
+                  
+
+                  <div>
+                    <div>
+                      Python
+                    </div>
+                    <div className='inline-block'>
+                      <Box sx={{ zIndex:'5', width: 400 , height: 10 }}>
+                        <LinearProgress value={88} variant="determinate" />
+                      </Box>
+                    </div>
+                    <p className='inline-block ml-5'>88%</p>
+
+                  </div>
 
 
-                <div>
-                  JS/TS/React
-                  <Box sx={{ zIndex:'5', width: 500 , height: 10 }}>
-                    <LinearProgress value={50} variant="determinate" />
-                  </Box>
-                </div>
+                  <div>
+                    <div>
+                      JS/TS/React
+                    </div>
+                    <div className='inline-block'>
+                      <Box sx={{ zIndex:'5', width: 400 , height: 10 }}>
+                        <LinearProgress value={77} variant="determinate" />
+                      </Box>
+                    </div>
+                    <p className='inline-block ml-5'>77%</p>
+
+                  </div>
 
 
-                <div>
-                  Java
-                  <Box sx={{ zIndex:'5', width: 500 , height: 10 }}>
-                    <LinearProgress value={30} variant="determinate" />
-                  </Box>
-                </div>
+                  <div>
+                    <div>
+                      Java
+                    </div>
+                    <div className='inline-block'>
+                      <Box sx={{ zIndex:'5', width: 400 , height: 10 }}>
+                        <LinearProgress value={67} variant="determinate" />
+                      </Box>
+                    </div>
+                    <p className='inline-block ml-5'>67%</p>
 
+                  </div>
 
-                <div>
-                  C++
-                  <Box sx={{ zIndex:'5', width: 500 , height: 10 }}>
-                    <LinearProgress value={20} variant="determinate" />
-                  </Box>
-                </div>
+                  <div>
+                    <div>
+                      C++
+                    </div>
+                    <div className='inline-block'>
+                      <Box sx={{ zIndex:'5', width: 400 , height: 10 }}>
+                        <LinearProgress value={56} variant="determinate" />
+                      </Box>
+                    </div>
+                    <p className='inline-block ml-5'>56%</p>
+
+                  </div>
               </div>
             </div>
 
 
-            <div className='SkillsBlock'>
-              <h2>Profile</h2>
-              <p>Currently looking for a internship. If you think I'm a good fit, please contant me via joey.lamm@outlook.com.</p>
-            </div>
+            
           </div>
         </div>
 
-        <div id="Resume" className="block">
-          <h1 >Resume</h1>
+        <div id="Resume" className="h-[100vh] bg-[#0A0F2B]">
+          <h1>Resume</h1>
+
         </div>
 
         <div id="Projects" className="block">
           <h1>Projects</h1>
         </div>
 
-        <div id="Contact" className="block">
+        <div id="Contact" className="">
           <h1>Contact</h1>
         </div>
       
